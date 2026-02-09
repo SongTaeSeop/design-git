4eb4ab8da260ad4a61f9a870fada4f4e6b4514a9
송태섭 <xotjq237@gmail.com>
2026-02-09 16:55:47 +0900

    2026년 2월 9일 삼성꿈장학재단 biz 중간 마무리

--- sdream/js/main.js
+++ sdream/js/main.js
@@ -10,4 +10,35 @@ $(document).ready(function() {
 
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
     }); //new Swiper()
+
+    /* 1. .header에 마우스를 올리면 over 클래스 추가, 내리면 삭제 */
+    /* 2. 브라우저 스크롤을 아래로 내리면 .header에 fixed 클래스 추가, 브라우저를 다시 최상단으로 올리면 삭제 */
+    $('.header').on('mouseenter', function() {
+        // console.log(1);
+        $(this).addClass('over');
+    });
+    $('.header').on('mouseleave', function() {
+        $(this).removeClass('over');
+    });
+    
+    let scrolling // 브라우저가 스크롤된 값
+
+    function header_fixed() {
+        scrolling = $(window).scrollTop();
+        // console.log(scrolling);
+        if (scrolling > 0) {
+            // console.log('fixed (gt 0)');
+            $('.header').addClass('fixed');
+        } else {
+            // console.log('no fixed (leq 0)');
+            $('.header').removeClass('fixed');
+        }
+    }
+
+    header_fixed() // 맨 처음 브라우저 로딩된 이후 한 번
+    $(window).scroll(function() {
+        header_fixed(); // 브라우저가 스크롤될 때마다 실행
+    });
+
+
 }); //$(document).ready()
