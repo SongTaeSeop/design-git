fb4ef6c1172eda77edb3a18cdae5d7911ee7a6ef
송태섭 <xotjq237@gmail.com>
2026-02-23 18:00:18 +0900

    2026년 2월 23일 을유문화사 visual 마무리, header CSS 중간 마무리

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -1,115 +1,57 @@
-$(document).ready(function(){
-    const visual_swiper = new Swiper('.visual  .swiper', { /* 팝업을 감싼는 요소의 class명 */
+$(document).ready(function() {
+    let visual_bar_w;
+
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+
         autoplay: {  /* 팝업 자동 실행 */
             delay: 5000,
-            disableOnInteraction: true,
+            disableOnInteraction: false,
         },
-        //effect: "fade", /* fade 효과 */
+
+        effect: "fade", /* fade 효과 */
+
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    });//visual_swiper
 
-
-    
-    
-    /***********************************************************
-     * PC 메뉴
-     * .header .gnb .gnb_wrap ul.depth1 > li 여기에 마우스를 올리면 over 추가
-     * 조건: over는 마우스를 올린 단 하나의 li에만 추가해야 함
-     * 
-     * .header에 마우스를 올리면 menu_over 추가
-     * 조건: PC 버전
-    **********************************/
-    let win_w; // 브라우저 너비 값
-    let device_status; // pc인지 모바일인지 저장
-    let mobile_size = 1024; // 1024부터 모바일(메뉴)
-
-    function device_chk() {
-        win_w = $(window).width();
-        // console.log(win_w);
-        if (win_w > mobile_size) {
-            device_status = 'pc';
-        } else {
-            device_status = 'mobile';            
-        }
-        console.log(device_status);
-    }
-
-    device_chk();
-    $(window).resize(function() {
-        // 브라우저가 resize될 때마다 1번 실행
-        device_chk();
-    });
-
-
-
-
-    $('.header .gnb .gnb_open').on('click', function() {
-        if (device_status == 'mobile') {
-            $('.header').addClass('menu_open');
-        }
-    });
-    
-    $('.header .gnb .gnb_wrap .gnb_close').on('click', function() {
-        if (device_status == 'mobile') {
-            $('.header').removeClass('menu_open');
-        }
-    });
-    
-    $('.header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function() {
-        if (device_status == 'pc') {
-            $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('over');
-            $(this).addClass('over');
-            $('.header').addClass('menu_over');
-        }
-    });
-
-    $('.header .gnb .gnb_wrap ul.depth1 > li').on('click', function() {
-        if (device_status == 'mobile') {
-            $('.header .gnb .gnb_wrap ul.depth1 > li').removeClass('open');
-            $(this).addClass('open');
-        }
-    });
-
-    $('.header .gnb_bg').on('mouseenter', function() {
-        $('.header').removeClass('menu_over');
-    });
-
-    /***********************************************************
-     * 웹진 swiper
-    **********************************/
-    let scrolling //브라우저가 스크롤 된 값
-
-    const webzine_swiper = new Swiper('.webzine .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        breakpoints: {
-            // 541: {    /* 768-540사이 */
-            //     slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-            //     spaceBetween: 16,
-            // },
-            769: {    /* 768px 이상일때 적용 */
-                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 24,
+        pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
+            el: '.swiper-pagination', /* 해당 요소의 class명 */
+            clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
+            type: 'fraction',  /* type fraction을 주면 paging이 숫자로 표시됨 */
+            renderBullet: function (index, className) {   /* paging에 특정 코드 넣기 */
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
             },
         },
-        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
-        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-        navigation: {
-            nextEl: '.webzine .ctrl_wrap .next',
-            prevEl: '.webzine .ctrl_wrap .prev',
+        
+
+        navigation: {  /* 이전, 다음 버튼 */
+            nextEl: '.visual .ctrl_right .next',  /* 다음 버튼의 클래스명 */
+            prevEl: '.visual .ctrl_right .prev',  
+        },
+
+        on: {
+            autoplayTimeLeft(s, time, progress) {
+                // console.log(progress);
+                visual_bar_w = 100 - (100 * progress);
+                $('.visual .ctrl_left .bar span').width(visual_bar_w + '%');
+            }
         },
     });
 
-    /*******************************************
-     * top버튼을 클릭하면 상단으로(맨위로) 스크롤 
-    *****************************************/
-    $('.footer .top').on('click', function(){
-        console.log('클릭')
-        //$(window).scrollTop(0)
-        $('html,body').animate({
-            scrollTop : 0
-        }, 500)
-    })
-    
+    $('.visual .ctrl_left .stop').on('click', function() {
+        visual_swiper.autoplay.stop();  /* 일시정지 기능 */
+        $(this).hide();
+        $('.visual .ctrl_left .play').show();
+    });
+    $('.visual .ctrl_left .play').on('click', function() {
+        visual_swiper.autoplay.start();  /* 재생 기능 */
+        $(this).hide();
+        $('.visual .ctrl_left .stop').show();
+    });
 
-})//$(document).ready
+    /*** .menu_over ***/
+    $('.header .gnb .gnb_wrap ul.depth1 > li').on('mouseenter', function() {
+        $(this).addClass('over');
+    });
+    $('.header .gnb .gnb_wrap ul.depth1 > li').on('mouseleave', function() {
+        $(this).removeClass('over');
+    });
+});
