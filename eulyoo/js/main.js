415470eaa12de47d2b8563736db914ca2b6fc689
송태섭 <xotjq237@gmail.com>
2026-02-25 18:00:48 +0900

    2026년 2월 25일 book 마무리 interview 중간 마무리

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -12,6 +12,10 @@ $(document).ready(function() {
 
         loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 
+        loopPreventsSliding: false, /* loop 적용 시 딜레이가 추가됨 */
+
+        // speed: 1000,
+
         pagination: {  /* 몇개의 팝업이 있는지 보여주는 동그라미 */
             el: '.swiper-pagination', /* 해당 요소의 class명 */
             clickable: true,  /* 클릭하면 해당 팝업으로 이동할 것인지 값 */
@@ -92,6 +96,11 @@ $(document).ready(function() {
             $('.header').removeClass('menu_over');
         }
     });
+    $('.header').on('mouseleave', function() {
+        if (device_status == 'PC') {
+            $('.header').removeClass('menu_over');
+        }
+    });
 
     $('.header .util .search .search_open').on('focusin', function() {
         if (device_status == 'PC') {
@@ -166,5 +175,26 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
         scroll_chk();
+    });
+
+    /***
+     * .book Swiper 팝업
+     * ***/
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            769: {    /* 640px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        // loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        navigation: {
+            nextEl: '.book .next',
+            prevEl: '.book .prev',
+        },
     });
 });
