66616f10b416fc1cdebd8372ea3aa37112051cd1
송태섭 <xotjq237@gmail.com>
2026-03-13 16:56:20 +0900

    2026년 3월 13일 생명의숲 (footer 제외) 마무리

--- forest/js/main.js
+++ forest/js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
 			} else if (destination.index == 2) {
 				// console.log('3번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', '');
-				$('.header').attr('data-bg', '');
+				$('.header').attr('data-bg', 'mo');
 			} else if (destination.index == 3) {
 				// console.log('4번째 슬라이드가 로딩 되었을때');
 				$('.sec_nav').attr('data-bg', 'white');
@@ -72,5 +72,30 @@ $(document).ready(function() {
 		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 	});
 
+	/* .story의 Swiper */
+	const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+		slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+		spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+		breakpoints: {						
+			283: {    /* 283px 이상일때 적용 */
+				slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 16,
+			},		
+			451: {    /* 451px 이상일때 적용 */
+				slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 16,
+			},		
+			769: {    /* 769px 이상일때 적용 */
+				slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+			1281: {    /* 1281px 이상일때 적용 */
+				slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+				spaceBetween: 24,
+			},
+		},
+		//centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+		loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+	});
 
 }); // $(document).ready()
