1b22a4ddf69c9d5cd07d062584095556033bb449
송태섭 <xotjq237@gmail.com>
2026-03-19 18:00:42 +0900

    일단 모바일 메뉴 중간

--- kahm/js/common.js
+++ kahm/js/common.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-    let language_list
-    $('.header .util .language_open').click(function() {
+    const language_btn = $('.header .util .language_open');
+    const language_btn_sitemap = $('.header .sitemap .util_mobile .language_open');
+    
+    let language_list;
+
+    language_btn.click(function() {
+        language_list = $(this).next();
+        if (language_list.hasClass('open')) {
+            language_list.removeClass('open');
+        } else {
+            language_list.addClass('open');
+        }
+    });
+
+    language_btn_sitemap.click(function() {
         language_list = $(this).next();
         if (language_list.hasClass('open')) {
             language_list.removeClass('open');
