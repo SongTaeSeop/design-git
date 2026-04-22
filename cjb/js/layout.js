b46be70394cb4a3a2ef3eda1efa5d9dbe89fdc54
송태섭 <xotjq237@gmail.com>
2026-04-22 18:01:15 +0900

    2026년 4월 22일 CJB news까지 마무리

--- cjb/js/layout.js
+++ cjb/js/layout.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     let isTop;
     let isDesktop;
-    const breakpoint = 768;
+    const breakpoint = 1024;
 
     function check_desktop() {
         isDesktop = ($(window).width() > breakpoint);
@@ -18,7 +18,7 @@ $(document).ready(function() {
         isTop = ($(window).scrollTop() <= 0);
         if ((isTop)) {
             $('.header').removeClass('scroll');
-        } else if (isDesktop) {
+        } else {
             $('.header').addClass('scroll');
         }
     }
@@ -27,6 +27,52 @@ $(document).ready(function() {
 
     $(window).scroll(function() {
         check_top();
+    });
+
+    function toggleClass($target, className) {
+        if (!className || typeof className !== 'string') return;
+
+        const $els = ($target instanceof jQuery) ? $target : $($target);
+        $els.each(function() {
+            const $el = $(this);
+            if ($el.hasClass(className)) {
+            $el.removeClass(className);
+            } else {
+            $el.addClass(className);
+            }
+        });
+    }
+
+    /***
+     * 사이트맵 열기 접기
+     ***/
+
+    const sitemap_open_btn = $('.header .left .gnb .sitemap_open');
+    const sitemap_close_btn = $('.header .sitemap_wrap .util .sitemap_close');
+    const sitemap_wrap = $('.header .sitemap_wrap');
+
+    sitemap_open_btn.click(function() {
+        if (!sitemap_wrap.hasClass('open')) {
+            sitemap_wrap.addClass('open');
+        }
+    });
+    sitemap_close_btn.click(function() {
+        if (sitemap_wrap.hasClass('open')) {
+            sitemap_wrap.removeClass('open');
+        }
+    });
+
+    /***
+     * 모바일) 메뉴 열기 접기
+     ***/
+    const depth1_btn = $('.header .sitemap_wrap ul.depth1 > li > a');
+    depth1_btn.click(function(e) {
+        if (!isDesktop) {
+            if ($(this).next().is('ul.depth2')) {
+                e.preventDefault();
+                toggleClass($(this).parent(), 'open');
+            }
+        }
     });
 
 });
--- cjb/js/main.js
+++ cjb/js/main.js
@@ -62,4 +62,63 @@ $(document).ready(function() {
         visual_tab_conts.removeClass('active');
         $('#' + selected_tab).addClass('active');
     });
+
+    /***
+     * .news tab 버튼
+     * 1. 선택한 li에 active를 주고 나머지는 제거
+     * 2. 선택한 li 버튼 내용 뒤로 <span class="sr-only"> 선택됨</span> 추가하고 나머지는 제거
+     * 3. 선택한 li의 aria-selected를 true로 하고 나머지는 false로 설정
+     * 4. 선택한 li의 aria-controls에 해당하는 클래스를 tab-conts에서 찾아 active 클래스를 주고 나머지는 제거
+     * ***/
+
+    const news_tab_button = $('.news .contents_box .tab li');
+    const news_tab_conts = $('.news .contents_box .tab-conts .tab-contents');
+
+    news_tab_button.click(function() {
+        news_tab_button.removeClass('active');
+        $(this).addClass('active');
+        
+        news_tab_button.find('.sr-only').remove();
+        $(this).find('.tab-item').append(sr_only_selected);
+
+        news_tab_button.attr('aria-selected', 'false');
+        $(this).attr('aria-selected', 'true');
+
+        selected_tab = $(this).attr('aria-controls');
+
+        news_tab_conts.removeClass('active');
+        $('#' + selected_tab).addClass('active');
+
+        news_tab_conts.removeClass('active');
+        $('#' + selected_tab).addClass('active');
+    });
+
+    /* 모바일) 강조된 탭 아이템으로 스크롤 */
+    const tab_list = $('.news .contents_box .tab ul li');
+
+    var mut = new MutationObserver(function(mutations, mut){
+        if (mutations.length > 1) {
+            filtered = Array.from(mutations).filter((dom) => (dom.target.className == 'active'));
+            target = $(filtered[0].target);
+            target.parent()[0].scrollTo({
+                left: target[0].offsetLeft - 16,
+                behavior: 'smooth'
+            });
+        }
+    });
+
+    tab_list.each(function(idx, element) {
+        mut.observe(element, {
+            'attributes': true,
+            'attributeFilter': ['class']
+        });
+    });
+
+    const vod_swiper = new Swiper('.vod .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        // effect: "fade", /* fade 효과 */
+
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+
+        loopPreventsSliding: false, /* loop 적용 시 딜레이가 추가됨 */
+    });
 });
