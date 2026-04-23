af18708f9a5c3541ada79b798a15fe1a58b67421
송태섭 <xotjq237@gmail.com>
2026-04-23 18:00:32 +0900

    2026년 4월 23일 CJB radio 중간 마무리

--- cjb/js/main.js
+++ cjb/js/main.js
@@ -1,4 +1,29 @@
 $(document).ready(function() {
+    let isDesktop;
+    let isTablet;
+
+    const breakpoint1 = 1024;
+    const breakpoint2 = 768;
+
+    function check_desktop() {
+        isDesktop = ($(window).width() > breakpoint1);
+        isTablet = ($(window).width() > breakpoint2);
+    }
+
+    check_desktop();
+
+    $(window).resize(function() {
+        check_desktop();
+    });
+
+    function swiper_pc_only(s, target_device) {
+        if (target_device) {
+            s.enable();
+        } else {
+            s.disable();
+        }
+    }
+    
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         autoplay: {  /* 팝업 자동 실행 */
@@ -116,9 +141,120 @@ $(document).ready(function() {
 
     const vod_swiper = new Swiper('.vod .swiper', { /* 팝업을 감싼는 요소의 class명 */
         // effect: "fade", /* fade 효과 */
-
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-
-        loopPreventsSliding: false, /* loop 적용 시 딜레이가 추가됨 */
+        spaceBetween: 24,
+        navigation: {
+            nextEl: '.vod .contents_box .next',
+            prevEl: '.vod .contents_box .prev',
+        },
     });
+
+    swiper_pc_only(vod_swiper, isDesktop);
+
+    $(window).resize(function() {
+        swiper_pc_only(vod_swiper, isDesktop);
+    });
+
+    const vod_more_btn = $('.vod .contents_box .more');
+    const vod_first_swiper_slide = $('.vod .contents_box .swiper-slide').first();
+    
+    vod_more_btn.click(function() {
+        if (!isDesktop && !vod_first_swiper_slide.hasClass('open')) {
+            vod_first_swiper_slide.find('.vod_grid').addClass('open');
+            vod_more_btn.hide();
+        }
+    });
+
+
+    /***
+     * .program Swiper
+     ***/
+
+    const program_swiper = new Swiper('.program .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: "auto", /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 0, /* 팝업과 팝업 사이 여백 */
+        observeSlideChildren: true, 
+        breakpoints: {
+            769: {
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: '.program .contents_box .next',
+            prevEl: '.program .contents_box .prev',
+        },
+    });
+
+    swiper_pc_only(program_swiper, isTablet);
+
+    $(window).resize(function() {
+        swiper_pc_only(program_swiper, isTablet);
+    });
+
+    const program_more_btn = $('.program .contents_box .more');
+    const program_list = $('.program .contents_box .swiper-wrapper');
+    
+    program_more_btn.click(function() {
+        if (!isTablet && !program_list.hasClass('open')) {
+            program_list.addClass('open');
+            program_more_btn.hide();
+        }
+    });
+
+    function swiper_overflow_check(s,target, className) {
+        if (!s.isLocked) {
+            target.addClass(className);
+        } else {
+            target.removeClass(className);
+        }
+    }
+
+    /***
+     * .program .tab 버튼으로 Swiper 필터링
+     * 1. 선택한 li에 active를 주고 나머지는 제거
+     * 2. 선택한 li 버튼 내용 뒤로 <span class="sr-only"> 선택됨</span> 추가하고 나머지는 제거
+     * 3. 선택한 li의 aria-pressed true로 하고 나머지는 false로 설정
+     * 4. 선택한 날짜에 맞는 program_list 내 li을 show하고 나머지는 hide
+     * ***/
+
+    const program_tab_btn = $('.program .tab li');
+
+    program_tab_btn.click(function() {
+        program_tab_btn.removeClass('active');
+        $(this).addClass('active');
+        
+        program_tab_btn.find('.sr-only').remove();
+        $(this).find('.tab-item').append(sr_only_selected);
+
+        program_tab_btn.attr('aria-pressed', 'false');
+        $(this).attr('aria-pressed', 'true');
+
+        selected_filter = $(this).attr('id');
+        if (selected_filter == 'all') {
+            program_list.children().show();
+            if (!isTablet) {
+                program_list.removeClass('open');
+                program_more_btn.show();
+            }
+        } else {
+            program_list.children().hide(); // 전체 숨기기
+            program_list.children('[data-day~="' + selected_filter + '"]').show();
+        }
+        program_swiper.update();
+        swiper_overflow_check(program_swiper, $('.program .swiper'), 'drag');
+    });
+
+    swiper_overflow_check(program_swiper, $('.program .swiper'), 'drag');
+
+    $(window).on('pointermove mousemove touchmove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
+        $('.cursor').css('left', e.pageX + 'px');
+        $('.cursor').css('top', e.pageY + 'px');
+    });
+
+    $('.program .swiper').hover(function() { /* 특정한 요소에 마우스를 올렸을때만 on 클래스 주기 */
+        if (isTablet && $(this).hasClass('drag')) {
+            $('.cursor').toggleClass('on');
+        }
+    });
+
 });
