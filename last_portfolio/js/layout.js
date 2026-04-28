81b72ca36e9c3eb61df9baf691cbfbd0bd76947e
송태섭 <xotjq237@gmail.com>
2026-04-28 18:01:37 +0900

    2026년 4월 28일 portfolio 이제 시작

---
+++ last_portfolio/js/layout.js
@@ -0,0 +1,111 @@
+$(document).ready(function() {
+
+    let isTop;
+    let isDesktop;
+    const breakpoint = 768;
+
+    function check_desktop() {
+        isDesktop = ($(window).width() > breakpoint);
+    }
+
+    check_desktop();
+
+    $(window).resize(function() {
+        check_desktop();
+    });
+
+    /*
+    * check_top:
+    * 1. PC) 만약 화면 최상단에 있으면: fixed를 제거한다.
+    * 2. Mobile) 만약 화면 최상단에 있으면: fixed를 제거한다.
+    * 3. PC) 만약 화면을 스크롤하면: fixed를 추가한다.
+    * 4. Mobile) 만약 화면을 스크롤하면: fixed를 추가하지 않는다.
+    */
+
+    function check_top() {
+        isTop = ($(window).scrollTop() <= 0);
+        if ((isTop)) {
+            $('.header').removeClass('fixed');
+        } else if (isDesktop) {
+            $('.header').addClass('fixed');
+        }
+    }
+
+    check_top();
+
+    $(window).scroll(function() {
+        check_top();
+    });
+
+    /*
+    내부 링크 버튼:
+    클릭하면 아래로 스크롤
+    */
+
+    function scrollToElement(elementSelector, instance = 0) {
+        const elements = document.querySelectorAll(elementSelector);
+        if (elements.length > instance) {
+            elements[instance].scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    }
+    $('.header .gnb ul li a').click(function(e) {
+        e.preventDefault();
+        console.log($(this).attr('href'));
+        scrollToElement($(this).attr('href'));
+    });
+
+    /*
+    화면 스크롤에 따른 탭 강조 갱신
+    */
+
+    const sections = $('section');
+    let old_id;
+    let id;
+
+    const observer = new window.IntersectionObserver(([entry]) => {
+
+        if (entry.isIntersecting) {
+            target = entry.target;
+            old_id = id;
+            id = $(target).attr('data-section');
+            // console.log(old_id, id);
+            if (old_id != id) {
+                $('.header .gnb ul li a[href="#' + old_id + '"]').removeClass('active');
+                $('.header .gnb ul li a[href="#' + id + '"]').addClass('active');
+                $('.gnb_mobile ul li a[href="#' + old_id + '"]').removeClass('active');
+                $('.gnb_mobile ul li a[href="#' + id + '"]').addClass('active');
+            }
+        }
+        }, {
+            root: null,
+            threshold: 0.1, // set offset 0.1 means trigger if atleast 10% of element in viewport
+    });
+
+    sections.each(function(idx, element) {
+        observer.observe(element);
+    });
+
+    /* 모바일) 강조된 탭 아이템으로 스크롤 */
+
+    const gnb_items = $('.gnb_mobile ul li a');
+
+    var mut = new MutationObserver(function(mutations, mut){
+        if (mutations.length > 1) {
+            filtered = Array.from(mutations).filter((dom) => (dom.target.className == 'active'));
+            target = $(filtered[0].target).parent();
+            target.parent()[0].scrollTo({
+                left: target[0].offsetLeft,
+                behavior: 'smooth'
+            });
+        }
+    });
+
+    gnb_items.each(function(idx, element) {
+        mut.observe(element, {
+            'attributes': true,
+            'attributeFilter': ['class']
+        });
+    });
+});
---
+++ last_portfolio/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+    AOS.init({
+        offset: ($(window).height() / 2),
+        duration: 500,
+        once: true
+    });
+    function toggleClass($target, className) {
+        if (!className || typeof className !== 'string') return;
+
+        const $els = ($target instanceof jQuery) ? $target : $($target);
+        $els.each(function() {
+            const $el = $(this);
+            if ($el.hasClass(className)) {
+            $el.removeClass(className);
+            } else {
+            $el.addClass(className);
+            }
+        });
+    }
+
+    /*
+    내부 링크 버튼:
+    클릭하면 아래로 스크롤
+    탭 강조 효과는 layout.js에서
+    */
+
+    function scrollToElement(elementSelector, instance = 0) {
+        const elements = document.querySelectorAll(elementSelector);
+        if (elements.length > instance) {
+            elements[instance].scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+    }
+    $('.gnb_mobile ul li a').click(function(e) {
+        e.preventDefault();
+        scrollToElement($(this).attr('href'));
+    })
+    
+    /*
+    faq 아코디언
+    버튼을 클릭하면:
+    */
+    $('.faq .contents_box .item .accordion-button').click(function() {
+        // 1. active 클래스 줌
+        toggleClass($(this), 'active');
+        toggleClass($(this).parent().parent(), 'active');
+        // 2. aria-expanded를 True로
+        if ($(this).attr('aria-expanded') == 'true') {
+            $(this).attr('aria-expanded', 'false');            
+        } else {
+            $(this).attr('aria-expanded', 'true');
+        }
+        // 3. 내용 표시 또는 숨김
+        
+    });
+
+    /*
+    Top 버튼
+    */
+   $('.util .top').click(function() {
+        $('html,body').stop().animate({
+            scrollTop: 0
+        }, 300);
+   });
+
+   
+});
